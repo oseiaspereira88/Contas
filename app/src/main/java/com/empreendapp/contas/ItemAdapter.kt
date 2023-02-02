601670e428de1db7630a817e99a2a224f54cd6f5
Oseias Pereira <oseiaspereira88@gmail.com>
2023-02-02 02:41:26 -0300

    Initial commit

---
+++ app/src/main/java/com/empreendapp/contas/ItemAdapter.kt
@@ -0,0 +1,24 @@
+package com.empreendapp.contas
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class ItemAdapter : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ItemViewHolder(itemView: View) : ViewHolder(itemView) {
+        
+    }
+}
